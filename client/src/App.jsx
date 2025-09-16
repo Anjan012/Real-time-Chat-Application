e2397a216e2e2a3e3ac5f29c1bd8009fbddbe432
Anjan012 <anjankhadka199@gmail.com>
2025-09-16 19:21:32 +0545

    Environment setup and started the project

---
+++ client/src/App.jsx
@@ -0,0 +1,27 @@
+import { useState } from 'react'
+import './App.css'
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { Auth } from "./pages/auth"
+import { Chat } from "./pages/chat"
+import { Profile } from "./pages/profile"
+
+
+const App = () => {
+  return (
+    <div>
+      <h1>App is working!</h1>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/auth" element={<Auth/>} />
+          <Route path="/" element={<div><h2>Home Page</h2><p>Go to <a href="/auth">/auth</a></p></div>} />
+          <Route path="/chat" element={<Chat/>} />
+          <Route path="/profile" element={<Profile/>} />
+
+          <Route path="*" element={<Navigate to="/auth" />} /> 
+        </Routes>
+      </BrowserRouter>
+    </div>
+  )
+}
+
+export default App
---
+++ client/src/pages/chat/index.jsx
@@ -0,0 +1,8 @@
+export const Chat = () => {
+  return (
+    <div>
+      <h1>Chat Page</h1>
+      <p>This is the Chat page!</p>
+    </div>
+  )
+}
